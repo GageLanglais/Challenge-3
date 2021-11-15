846cb3536b67fb29ad349be213cefa5ac7a87332
GageLanglais <89534501+GageLanglais@users.noreply.github.com>
2021-11-14 23:49:06 -0500

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+    
+    void Update()
+    {
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        HardEnemyController f = other.collider.GetComponent<HardEnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+    
+        Destroy(gameObject);
+    }
+}
